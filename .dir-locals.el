9a39887d3472063e317e0687d5ecea6c326fea0e
mavnn <michael@mavnn.co.uk>
2025-09-08 17:17:01 +0200

    Next step of the "org-roaming"

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,7 @@
-((nil . ((org-roam-directory . "/Users/mavnn/Documents/Web/blog/")
-         (org-roam-db-location . "/Users/mavnn/Documents/Web/blog/org-roam.db"))))
+((nil . ((eval . (set (make-local-variable 'this-directory)
+                      (file-name-directory
+                       (let ((d (dir-locals-find-file "./")))
+                         (if (stringp d) d (car d))))))
+         (eval . (setq-local org-roam-directory this-directory))
+         (eval . (setq-local org-roam-db-location (concat this-directory "/org-roam.db")))
+         )))
